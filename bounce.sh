03184b41a623db686a99e34310cb373b4d95d3a4
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-26 17:38:08 -0500

    Update bounce.sh

--- bounce.sh
+++ bounce.sh
@@ -1,1 +1,4 @@
-
+# D. On your Ubuntu client, using the GUI interface, navigate to the WIRED settings on the ethernet interface.  Under 
+# the main tab of DETAILS, be sure the “Connect Automatically” option is checked.  This will ensure that the 
+# ethernet kernel driver remains persistently active.  Next write a simple BASH script called bounce.sh that will 
+# shut down and then bring back up the interface (presumably called ens33) 
