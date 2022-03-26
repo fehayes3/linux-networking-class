316aaacab04e07a48e897b6675b2bcd3ff33191c
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-26 17:53:30 -0500

    Update makeRouter.sh

--- makeRouter.sh
+++ makeRouter.sh
@@ -1,1 +1,7 @@
-
+# On slide 16 and 17 of the Chapter 13 Networking course slide deck there are commands that can be used to 
+# convert a Linux Host into a forwarding router.  Write a script called makeRouter.sh that will perform this 
+# conversion.  The commands you would use are shown below. Argument 1 in the external network interface (e.g. 
+# NAT interface) and Argument 2 is the internal network interface (e.g. LAN segment 1)  You don’t have to test 
+# this script as we will continue editing this script in the next assignment.  If you do decide to test the script, be sure 
+# to create a SNAPshot of your Fedora Server before experimenting so that you can revert to your working system 
+# after testing. 
