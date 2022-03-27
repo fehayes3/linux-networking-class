6ac333bd64f1e00030268712a78a49a27e718007
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-27 18:30:43 -0500

    Update dh_fw.sh

--- dh_fw.sh
+++ dh_fw.sh
@@ -1,2 +1,8 @@
+ !/bin/bash 
 #A. Write a BASH script called dh_fw.sh that will start the DHCP service on the Fedora Server and invoke the 
 # IPTABLES network masquerading function on the primary Internet facing interface. 
+
+echo $(systemctl start dhcpd.service)
+
+echo $(iptables -t nat -A POSTROUTING -o ens160 -j MASQUERADE)  
+
