ddc1888bc243bc281d4398679cefdec8f2d726c3
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-27 19:21:40 -0500

    Update myserver.sh

--- myserver.sh
+++ myserver.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 # B. Write a BASH script called myserver.sh that changes the hostname of your FEDORA server to whatever 
 # argument you pass to the script.  For example,  <root#quantumforce># myserver.sh reflex  should modify the 
 # /etc/hostname file and change it from “quantumforce” to “reflex”
+echo Enter a New Host Name
+read varname
+
+echo$(sudo hostnamectl set-hostname $varname)
