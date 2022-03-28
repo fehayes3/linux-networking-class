e3fe0137f33fa955cb57d2ebb184c341af1b6f2d
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-27 19:44:23 -0500

    Update bounce.sh

--- bounce.sh
+++ bounce.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 # D. On your Ubuntu client, using the GUI interface, navigate to the WIRED settings on the ethernet interface.  Under 
 # the main tab of DETAILS, be sure the “Connect Automatically” option is checked.  This will ensure that the 
 # ethernet kernel driver remains persistently active.  Next write a simple BASH script called bounce.sh that will 
