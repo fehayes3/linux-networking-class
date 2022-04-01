42f3144efdac108803afb1ebfbdc875e3ea5972c
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-03-31 20:25:51 -0500

    Update serviceCheck.sh

--- serviceCheck.sh
+++ serviceCheck.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # C. Write a BASH script called serviceCheck.sh that will start the check and report the status for the DHCP service, 
 # the SSH service, and the VSFTP service on the Fedora Server.  Hint: use systemctl --no-pager option to allow the 
-# output to fully flow. 
+# output to fully flow.
+systemctl status dhcpd.service --no-pager
+sudo systemctl status sshd --no-pager
+systemctl status vsftpd --no-pager
