64ff97081b3bacebf3a1064fea985d92a0735922
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-04-11 21:49:56 -0500

    Create intfcAlias.sh

---
+++ intfcAlias.sh
@@ -0,0 +1,3 @@
+#a. Write a BASH script called intfcAlias.sh that will assign multiple IP addresses to the secondary server 
+#interface facing Lan Segment. The following IP addresses should be created: 
+#20.30.40.2/24,    20.30.40.23/24,     20.30.40.25/24,     20.30.40.80/24 
