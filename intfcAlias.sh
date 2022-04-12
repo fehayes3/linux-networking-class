e7050d78a7ae0a3f905e997fac79283de814e969
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-04-11 21:52:12 -0500

    Update intfcAlias.sh

--- intfcAlias.sh
+++ intfcAlias.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 #a. Write a BASH script called intfcAlias.sh that will assign multiple IP addresses to the secondary server 
 #interface facing Lan Segment. The following IP addresses should be created: 
 #20.30.40.2/24,    20.30.40.23/24,     20.30.40.25/24,     20.30.40.80/24 
+
+sudo ip addr add 192.168.2.105/24 dev ens224
+sudo ip addr add 192.168.2.105/24 dev ens224
+sudo ip addr add 192.168.2.105/24 dev ens224
