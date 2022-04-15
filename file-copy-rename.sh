ee891789fb56b6f9fb47fb78f359aa2447103154
fehayes3 <102438218+fehayes3@users.noreply.github.com>
2022-04-14 20:49:45 -0500

    Create file-copy-rename.sh

---
+++ file-copy-rename.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Create a BASH shell that will convert the DNS domain configuration files from the “example.org” 
+#domain to a domain specified as the input argument to the script.  For example, your script will replace all 
+#instances of example.org in the target files /var/named/example.org.db, /var/named/example.org.rev, and 
+#/etc/named.conf with YOUR domain name. The new files that are written should be specific to your new 
+#domain.  For example if the new domain is “quantum.net” you would write the new files called 
+#quantum.net.db and quantum.net.rev.  The original existing files (example.org.db, example.org.rev, and 
+#named.conf) should be preserved (not be overwritten or modified in any way). Consider testing your 
+#script on some sample files in a directory you create to verify the script is working properly before you try 
+#and modify the true target files. 
+
+
+
+echo Enter a New Domain Name
+read varname
+cp  /var/named/example.org.db /var/named/$varname.db
